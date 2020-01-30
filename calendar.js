981e4ee419ff9e84ae216f8fd0398237d70dba20
Grigory Kuimov <gk061090@gmail.com>
2020-01-30 21:27:30 +0300

    add calendar func

--- calendar.js
+++ calendar.js
@@ -1,1 +1,30 @@
+const dates = {
+  2019: {
+    12: [28, 29, 30]
+  },
+  2020: {
+    1: [2, 6, 9],
+    2: [2, 4, 5, 6]
+  }
+};
 
+const format = value => (value <= 9 ? `0${value}` : value);
+
+const prepareDates = dates => {
+  const strings = [];
+  Object.entries(dates).map(([year, monthWitDays]) => {
+    Object.entries(monthWitDays).map(([month, days]) => {
+      days.map(day => {
+        console.log(`${year}.${format(month)}.${format(day)}`);
+      });
+    });
+  });
+
+  return strings;
+};
+
+prepareDates(dates);
+
+// const formatedDates = prepareDates(dates);
+
+// console.log(formatedDates);
